96669c1dfb4f91322484f2f7cb07ac9b77f75586
Lucas Silva <lucasre.silva@gmail.com>
2019-10-10 22:00:53 -0300

    feat(Assets): app icon add

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
       title: 'Vetores',
+      color: Colors.redAccent,
       theme: CupertinoThemeData(
         primaryColor: Colors.redAccent,
         textTheme: CupertinoTextThemeData(
