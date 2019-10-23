c54911601efa0b0c0f0ca26fae610401d440dc41
Lucas Silva <lucasre.silva@hotmail.com>
2019-10-23 12:32:03 -0300

    feat(ui): novo icone

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:vetores/src/app.dart';
 
 Future main() async {
-  DotEnv().load('.env').then((env) {
-    Parse().initialize(
-        DotEnv().env['PARSE_APP_ID'], DotEnv().env['PARSE_APP_URL'],
-        clientKey: DotEnv().env['PARSE_APP_CLINT_KEY'],
-        debug: true);
-  });
+  await DotEnv().load('.env');
+  await Parse().initialize(
+    DotEnv().env['PARSE_APP_ID'],
+    DotEnv().env['PARSE_APP_URL'],
+    clientKey: DotEnv().env['PARSE_APP_CLINT_KEY'],
+  );
   runApp(MyApp());
 }
