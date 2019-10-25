95e2cf16dceafe284d81486f3aba82f0a54879fd
Lucas Silva <lucasre.silva@hotmail.com>
2019-10-24 21:59:27 -0300

    feat(Error): sentry add

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 import 'package:vetores/src/app.dart';
+import 'package:sentry/sentry.dart';
+
+final SentryClient sentry = SentryClient(
+  dsn: 'https://73a6456747ed4f7f8c89f5b572b432b8@sentry.io/1795676',
+);
 
 Future main() async {
-  await DotEnv().load('.env');
-  await Parse().initialize(
-    DotEnv().env['PARSE_APP_ID'],
-    DotEnv().env['PARSE_APP_URL'],
-    clientKey: DotEnv().env['PARSE_APP_CLINT_KEY'],
-    debug: true
-  );
-
-  runApp(MyApp());
+  try {
+    await DotEnv().load('.env');
+    await Parse().initialize(
+        DotEnv().env['PARSE_APP_ID'], DotEnv().env['PARSE_APP_URL'],
+        clientKey: DotEnv().env['PARSE_APP_CLINT_KEY'], debug: true);
+    runZoned(
+      () => runApp(MyApp()),
+      onError: (Object error, StackTrace stackTrace) {
+        try {
+          sentry.captureException(
+            exception: error,
+            stackTrace: stackTrace,
+          );
+          print('Error sent to sentry.io: $error');
+        } catch (e) {
+          print('Sending report to sentry.io failed: $e');
+          print('Original error: $error');
+        }
+      },
+    );
+  } catch (error, stackTrace) {
+    await sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+  }
 }
